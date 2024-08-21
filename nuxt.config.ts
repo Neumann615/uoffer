1f796a0e6d35223629343d4a8ad3dc81b3fbc3be
wxxFE <2022095028@qq.com>
2024-08-21 23:34:24 +0800

    变更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,16 +14,16 @@ export default defineNuxtConfig({
     },
     routeRules: {
         // 主页在构建时预渲染
-        '/': {prerender: true},
-        // 产品页面按需生成，后台自动重新验证
-        '/products/**': {swr: 3600},
-        // 博客文章按需生成，直到下一次部署前持续有效
-        '/blog/**': {isr: true},
-        // 管理仪表板仅在客户端渲染
-        '/about/**': {ssr: true},
-        // 在API路由上添加cors头
-        '/api/**': {cors: true},
-        // 跳转旧的URL
-        '/old-page': {redirect: '/new-page'}
+        //'/': {prerender: true},
+        // // 产品页面按需生成，后台自动重新验证
+        // '/products/**': {swr: 3600},
+        // // 博客文章按需生成，直到下一次部署前持续有效
+        // '/blog/**': {isr: true},
+        // // 管理仪表板仅在客户端渲染
+        // '/about/**': {ssr: true},
+        // // 在API路由上添加cors头
+        // '/api/**': {cors: true},
+        // // 跳转旧的URL
+        // '/old-page': {redirect: '/new-page'}
     }
 });
